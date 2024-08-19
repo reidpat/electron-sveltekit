8c116509609903f0b8c0db194524b1a2ff0de1ef
reidpat <61036489+reidpat@users.noreply.github.com>
2024-08-19 07:27:40 -0700

    Button Navigation & animations

---
+++ src/lib/NavigationCluster.svelte
@@ -0,0 +1,99 @@
+<script>
+    import { fade } from "svelte/transition";
+    import ChevronUp from "~icons/mynaui/chevron-up";
+    import ChevronDown from "~icons/mynaui/chevron-down";
+    import ChevronLeft from "~icons/mynaui/chevron-left";
+    import ChevronRight from "~icons/mynaui/chevron-right";
+
+    export let canMove;
+    export let changeCoordinates;
+</script>
+
+<div class="map-buttons">
+    <button class="left" on:click={() => changeCoordinates(-1, 0)}>
+        <div class="button-content">
+            {#if canMove(-1, 0)}
+                <div transition:fade={{ duration: 200 }}>
+                    <ChevronLeft />
+                </div>
+            {/if}
+        </div>
+    </button>
+    <button class="up" on:click={() => changeCoordinates(0, -1)}>
+        <div class="button-content">
+            {#if canMove(0, -1)}
+                <div transition:fade={{ duration: 200 }}>
+                    <ChevronUp />
+                </div>
+            {/if}
+        </div>
+    </button>
+    <button class="down" on:click={() => changeCoordinates(0, 1)}>
+        <div class="button-content">
+            {#if canMove(0, 1)}
+                <div transition:fade={{ duration: 200 }}>
+                    <ChevronDown />
+                </div>
+            {/if}
+        </div>
+    </button>
+    <button class="right" on:click={() => changeCoordinates(1, 0)}>
+        <div class="button-content">
+            {#if canMove(1, 0)}
+                <div transition:fade={{ duration: 200 }}>
+                    <ChevronRight />
+                </div>
+            {/if}
+        </div>
+    </button>
+</div>
+
+<style>
+    .map-buttons {
+        position: absolute;
+        right: 20px;
+        bottom: 20px;
+        width: 150px;
+        height: 150px;
+        display: grid;
+        grid-template-areas:
+            ". up ."
+            "left . right"
+            ". down .";
+        gap: 5px;
+        background-color: none;
+    }
+    button {
+        width: 50px;
+        height: 50px;
+        padding: 0;
+        background: none;
+        border: none;
+        color: var(--pico-primary);
+        cursor: pointer;
+        position: relative;
+    }
+    .button-content {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 2rem;
+    }
+    .left {
+        grid-area: left;
+    }
+    .up {
+        grid-area: up;
+    }
+    .down {
+        grid-area: down;
+    }
+    .right {
+        grid-area: right;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from "svelte";
     import DynamicFrame from "../lib/DynamicFrame.svelte";
+    import { fade } from 'svelte/transition';
 
     let fileName;
     let mapName = "frames-pitch";
@@ -59,6 +60,8 @@
                 break;
         }
     }
+
+    import NavigationCluster from "$lib/NavigationCluster.svelte";
 </script>
 
 <svelte:window on:keydown={handleKeyPress} />
@@ -68,44 +71,10 @@
         <DynamicFrame bind:fileName />
     {/if}
 </div>
-<div class="map-buttons">
-    <button class="outline {canMove(-1, 0) ? '' : 'secondary'}" on:click={() => changeCoordinates(-1, 0)}>Left</button>
-    <div class="map-buttons-center">
-        <button class="outline {canMove(0, -1) ? '' : 'secondary'}" on:click={() => changeCoordinates(0, -1)}>Up</button>
-        <button class="outline {canMove(0, 1) ? '' : 'secondary'}" on:click={() => changeCoordinates(0, 1)}>Down</button>
-    </div>
-    <button class="outline {canMove(1, 0) ? '' : 'secondary'}" on:click={() => changeCoordinates(1, 0)}>Right</button>
-</div>
+<NavigationCluster {canMove} {changeCoordinates}/>
 
 <style>
-    .slide-container{
+    .slide-container {
         height: 100vh;
-    }
-    .map-buttons {
-        display: flex;
-        justify-content: center;
-        gap: 1rem;
-        position: absolute;
-        right: 20px;
-        bottom: 20px;
-    }
-    button {
-        padding: 0.5rem 1rem;
-        font-size: 1rem;
-    }
-    button:hover {
-        background-color: #f0f0f0;
-    }
-    button:active {
-        background-color: #e0e0e0;
-    }
-    button:focus {
-        outline: none;
-    }
-
-    .map-buttons-center {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
     }
 </style>
